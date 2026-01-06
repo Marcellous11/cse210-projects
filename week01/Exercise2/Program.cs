93d1d9762adadbc12ad5f39ecfe0c5c6f351b798
Marcellous Curtis Jr <1mcj001@gmail.com>
2026-01-05 20:47:30 -0600

    Exercise 1 and 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        int gradePercentage = -1;
+        string letterGrade;
+        bool passed = true;
+
+      Console.Write("What was your grade percentage? ");    
+          
+        try{
+            gradePercentage = int.Parse(Console.ReadLine());
+        }catch{
+            Console.WriteLine($"The value you enter was not a valid grade percentage");
+        }
+
+        if(gradePercentage < 0 || gradePercentage > 100)
+        {
+            Console.WriteLine("Grade must be inbetween 0 and 100.");
+            return;
+        }
+
+        if(gradePercentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if(gradePercentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if(gradePercentage >= 70)
+        {
+            letterGrade = "C";
+        }
+       else if( gradePercentage >= 60)
+       {
+            letterGrade = "D";
+            passed = false;
+       }
+       else
+       {
+            letterGrade = "F";
+            passed = false;
+       }
+
+        Console.Write($"\nYour letter grade is {letterGrade}.");
+        if(passed){
+            Console.Write($" Congradulations!! You passed!!");
+        }
+        
     }
 }
