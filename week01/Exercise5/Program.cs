5b634cc5d49ced34b8142ac4d6697b7839876e81
Marcellous Curtis Jr <1mcj001@gmail.com>
2026-01-05 21:58:48 -0600

    Exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        string userName;
+        int userNumber;
+        int userNumberSquared;
+
+        DisplayWelcome();
+        userName = PromptUserName();
+        userNumber = PromptUserNumber();
+        userNumberSquared = SquareNumber(userNumber);
+        DisplayResult(userName,userNumberSquared);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to my program!!!!");
+    }
+
+    static string PromptUserName()
+    {   
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine(); 
+    }
+
+    static int PromptUserNumber()
+    {
+        int favNumber;
+        Console.Write("Please enter your favorite number: ");
+        while(true)
+        {
+            try
+            {
+                favNumber = int.Parse(Console.ReadLine());
+                return favNumber; 
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a whole number for favorite number.");
+            }
+        }
+    }
+    
+    static int SquareNumber(int userNumber)
+    {
+        return userNumber * userNumber;
+    }
+
+    static void DisplayResult(string userName, int number)
+    {   
+        Console.WriteLine($"{userName}, the squre of your number is {number}");
     }
 }
