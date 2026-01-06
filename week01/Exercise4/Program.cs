e8bb2f41b87bfd61cd065fa403c03e93de8cf69a
Marcellous Curtis Jr <1mcj001@gmail.com>
2026-01-05 21:22:03 -0600

    Exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> userNumbers = new List<int>();
+        int newNum = -1;
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when done.");
+        while(newNum != 0)
+        {
+            Console.Write("Enter Number: ");
+            try
+            {
+                newNum = int.Parse(Console.ReadLine());
+            }
+           catch
+           {
+                Console.WriteLine("Value number be a whole number");
+           }
+
+           if(newNum != 0)
+           {
+                userNumbers.Add(newNum);
+           }
+        }
+
+        Console.WriteLine($"Sum: {userNumbers.Sum()}");
+        Console.WriteLine($"Average: {userNumbers.Average()}");
+        Console.WriteLine($"Largest Number: {userNumbers.Max()}");
     }
 }
